001464d08aff7df2f3096034d04ee11e1a14c00c
TerryLun <tianweilun@yahoo.com>
2020-01-31 15:31:02 -0800

    hstack

--- img_play.py
+++ img_play.py
@@ -1,22 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('smallgray.png', 0)
+img = cv2.imread('smallgray.png', 1)
 cv2.imwrite('img_copy.png', img)
 
-arr = np.zeros((2,3))
+arr = np.zeros((10, 10), dtype=int)
+print(arr)
 
-# Printing type of arr object
-print("Array is of type: ", type(arr))
-
-# Printing array dimensions (axes)
-print("No. of dimensions: ", arr.ndim)
-
-# Printing shape of array
-print("Shape of array: ", arr.shape)
-
-# Printing size (total number of elements) of array
-print("Size of array: ", arr.size)
-
-# Printing type of elements in array
-print("Array stores elements of type: ", arr.dtype)
+arr2 = np.vstack((img, img))
+print(img)
+print(arr2)
