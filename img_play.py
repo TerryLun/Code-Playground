1e761feca0a36a573da50c204055572ef0ec5d94
TerryLun <tianweilun@yahoo.com>
2020-01-31 14:43:19 -0800

    initial commit

--- img_play.py
+++ img_play.py
@@ -1,5 +1,22 @@
 import cv2
-import numpy
+import numpy as np
 
 img = cv2.imread('smallgray.png', 0)
 cv2.imwrite('img_copy.png', img)
+
+arr = np.zeros((2,3))
+
+# Printing type of arr object
+print("Array is of type: ", type(arr))
+
+# Printing array dimensions (axes)
+print("No. of dimensions: ", arr.ndim)
+
+# Printing shape of array
+print("Shape of array: ", arr.shape)
+
+# Printing size (total number of elements) of array
+print("Size of array: ", arr.size)
+
+# Printing type of elements in array
+print("Array stores elements of type: ", arr.dtype)
