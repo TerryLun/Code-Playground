78c09b2558f9f0ea71832d8b863204fcdadd158f
TerryLun <tianweilun@yahoo.com>
2020-02-18 23:08:37 -0800

    practice

--- img_play.py
+++ img_play.py
@@ -5,26 +5,20 @@ import numpy as np
 import random
 
 
-def multiples_of_3(upperbound):
-    total = 0
-    for num in range(3, upperbound, 3):
-        total += num
-    return total
 
-
-def multiples(upperbound):
-    # n(a1+an)/2
-    # change upperbound to exclusive
-    upperbound -= 1
-    return (upperbound // 3) * (3 + (upperbound - upperbound % 3)) / 2
+def name_list():
+    names = {}
+    name = input('Enter a name: (\'quit\' to finish)')
+    while name != 'quit':
+        if len(name) not in names:
+            names[len(name)] = []
+        names[len(name)].append(name)
+        name = input('Enter a name: (\'quit\' to finish)')
+    return names
 
 
 def main():
-    for i in range(10, 100):
-        print('i = ', i)
-        print(multiples_of_3(i))
-        print(multiples(i))
-        print()
+    print(name_list())
 
 
 if __name__ == '__main__':
