9610bcd15680fd66fc5dca2fc2bfd7d782a3eacb
TerryLun <tianweilun@yahoo.com>
2020-02-20 00:46:25 -0800

    practice

--- img_play.py
+++ img_play.py
@@ -3,27 +3,11 @@ import copy
 import cv2 as cv
 import numpy as np
 import random
+import doctest
 
+res = {k: 0 for k in range(1, 7)}
+for _ in range(1_000_000):
+    res[random.randint(1, 6)] += 1
 
-def pre_pender(first, second):
-    count = 0
-    for num in first:
-        if num % second == 0 and num != 0:
-            count += 1
-    return count
-
-
-def unicode(s):
-    return [ord(c) for c in s]
-
-
-def main():
-    a = [1, 2, 3]
-    b = [2, 4, 2]
-    c = [a, b]
-    d = {k: v+t for k, v, t in c}
-    print(d)
-
-
-if __name__ == '__main__':
-    main()
+for k, v in res.items():
+    print('%d rolled %d times' % (k, v))
