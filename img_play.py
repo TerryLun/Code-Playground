d7d23472a0800f37ef5988df4c9fbce7aa516a6d
TerryLun <tianweilun@yahoo.com>
2020-02-20 16:15:09 -0800

    practice

--- img_play.py
+++ img_play.py
@@ -5,9 +5,4 @@ import numpy as np
 import random
 import doctest
 
-res = {k: 0 for k in range(1, 7)}
-for _ in range(1_000_000):
-    res[random.randint(1, 6)] += 1
 
-for k, v in res.items():
-    print('%d rolled %d times' % (k, v))
