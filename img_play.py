6d658aac98fea11341d6e4a4722a6d0c5aa0c017
TerryLun <tianweilun@yahoo.com>
2020-02-20 19:58:22 -0800

    practice

--- img_play.py
+++ img_play.py
@@ -6,3 +6,19 @@ import random
 import doctest
 
 
+def isPrime(n):
+    if n == 2:
+        return False
+    return True if 0 not in [n % i for i in range(2, n)] else False
+
+
+def summ(a, b):
+    ls = []
+    for n in range(a, b + 1):
+        if isPrime(n):
+            ls.append(n)
+            print(n)
+    return sum(ls)
+
+
+print(summ(10, 20))
