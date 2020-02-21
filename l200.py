c5ed5bcaf508a0c3a8ef4939c1a901da2c15d4ac
Terry <tianweilun@yahoo.com>
2020-02-21 15:04:41 -0800

    leetcode #200: number of islands (m)

---
+++ l200.py
@@ -0,0 +1,29 @@
+def numIslands(grid):
+    count = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == '1':
+                count += 1
+                dfs(grid, r, c)
+    return count
+
+
+def dfs(grid, r, c):
+    if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[r]) or grid[r][c] == '0':
+        return
+    grid[r][c] = '0'
+    dfs(grid, r + 1, c)
+    dfs(grid, r - 1, c)
+    dfs(grid, r, c + 1)
+    dfs(grid, r, c - 1)
+
+
+grid = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
+got = numIslands(grid)
+expected = 1
+print(got == expected)
+
+grid = [["1", "1", "0", "0", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "1", "0", "0"], ["0", "0", "0", "1", "1"]]
+got = numIslands(grid)
+expected = 3
+print(got == expected)
