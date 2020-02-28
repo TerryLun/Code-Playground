b73261ce07e3f6ca34b0f7472a662b401a2d516d
TerryLun <tianweilun@yahoo.com>
2020-02-27 23:22:53 -0800

    changed display character in the grid

--- eight_queen.py
+++ eight_queen.py
@@ -1,37 +1,33 @@
-from numpy import matrix
-import time
-
-
 def number_of_queens(bo):
     total = 0
     for r in bo:
-        total += r.count(1)
+        total += r.count('Q')
     return total
 
 
 def validate(bo, x, y):
     # row
-    if 1 in bo[x]:
+    if 'Q' in bo[x]:
         return False
     # col
     for r in bo:
-        if r[y] == 1:
+        if r[y] == 'Q':
             return False
     # to top left
     for i, j in zip(range(x, -1, -1), range(y, -1, -1)):
-        if board[i][j] == 1:
+        if board[i][j] == 'Q':
             return False
     # to top right
     for i, j in zip(range(x, -1, -1), range(y, 8, 1)):
-        if board[i][j] == 1:
+        if board[i][j] == 'Q':
             return False
     # to bottom left
     for i, j in zip(range(x, 8, 1), range(y, -1, -1)):
-        if board[i][j] == 1:
+        if board[i][j] == 'Q':
             return False
     # to bottom right
     for i, j in zip(range(x, 8, 1), range(y, 8, 1)):
-        if board[i][j] == 1:
+        if board[i][j] == 'Q':
             return False
     return True
 
@@ -42,14 +38,21 @@ def solve(bo):
 
     for r in range(8):
         for c in range(8):
-            if validate(bo, r, c) and bo[r][c] == 0:
-                bo[r][c] = 1
+            if validate(bo, r, c) and bo[r][c] == '_':
+                bo[r][c] = 'Q'
                 if solve(bo):
                     return True
-                bo[r][c] = 0
+                bo[r][c] = '_'
     return False
 
 
-board = [[0 for _ in range(8)] for _ in range(8)]
+def print_board(bo):
+    for r in bo:
+        for c in r:
+            print(c, end=' ')
+        print()
+
+
+board = [['_' for _ in range(8)] for _ in range(8)]
 solve(board)
-print(matrix(board))
+print_board(board)
