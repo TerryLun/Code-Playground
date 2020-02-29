567ff3d8675dd9ebf6594332a213939f7deccd64
TerryLun <tianweilun@yahoo.com>
2020-02-28 23:34:48 -0800

    Leetcode #1108. Defanging an IP Address

---
+++ lc1108e.py
@@ -0,0 +1,13 @@
+"""
+1108. Defanging an IP Address
+
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+A defanged IP address replaces every period "." with "[.]".
+"""
+
+def defangIPaddr(self, address):
+    """
+    :type address: str
+    :rtype: str
+    """
+    return address.replace('.', '[.]')
