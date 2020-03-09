3b4e65093fe272585a40092d84726997719515f5
TerryLun <tianweilun@yahoo.com>
2020-03-09 14:19:09 -0700

    Leetcode #953. Verifying an Alien Dictionary

--- lc953e.py
+++ lc953e.py
@@ -15,21 +15,41 @@ def isAlienSorted(words, order):
     :type order: str
     :rtype: bool
     """
-    alien_dict = {k: v for v, k in enumerate(order, 1)}
-    alien_dict[' '] = 0
-    longest = 0
-    for w in words:
-        longest = max(longest, len(w))
-    for i in range(len(words)):
-        words[i] = words[i].ljust(longest)
+    # alien_dict = {k: v for v, k in enumerate(order, 1)}
+    # alien_dict[' '] = 0
+    # longest = 0
+    # for w in words:
+    #     longest = max(longest, len(w))
+    # for i in range(len(words)):
+    #     words[i] = words[i].ljust(longest)
+    # for i in range(len(words) - 1):
+    #     word1 = words[i]
+    #     word2 = words[i+1]
+    #     for j in range(len(word1)):
+    #         if alien_dict[word1[j]] > alien_dict[word2[j]]:
+    #             return False
+    #         elif alien_dict[word1[j]] < alien_dict[word2[j]]:
+    #             break
+    # return True
+    order_index = {c: i for i, c in enumerate(order)}
+
     for i in range(len(words) - 1):
         word1 = words[i]
-        word2 = words[i+1]
-        for j in range(len(word1)):
-            if alien_dict[word1[j]] > alien_dict[word2[j]]:
-                return False
-            elif alien_dict[word1[j]] < alien_dict[word2[j]]:
+        word2 = words[i + 1]
+
+        # Find the first difference word1[k] != word2[k].
+        for k in range(min(len(word1), len(word2))):
+            # If they compare badly, it's not sorted.
+            if word1[k] != word2[k]:
+                if order_index[word1[k]] > order_index[word2[k]]:
+                    return False
                 break
+        else:
+            # If we didn't find a first difference, the
+            # words are like ("app", "apple").
+            if len(word1) > len(word2):
+                return False
+
     return True
 
 
