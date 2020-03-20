3f2d094bdfbeba0ea91a1bdcd25d76d2a64bbc78
TerryLun <tianweilun@yahoo.com>
2020-03-20 11:18:50 -0700

    Leetcode #46. Permutations using backtracking

--- lc46m.py
+++ lc46m.py
@@ -3,8 +3,7 @@
 
 Given a collection of distinct integers, return all possible permutations.
 """
-
-import itertools
+import copy
 
 
 def permute(nums):
@@ -12,4 +11,40 @@ def permute(nums):
     :type nums: List[int]
     :rtype: List[List[int]]
     """
-    return itertools.permutations(nums)
+
+    def helper(perm, usd):
+        if len(perm) == len(nums):
+            result.append(copy.deepcopy(perm))
+            return
+        for i in range(len(nums)):
+            if usd[i]:
+                continue
+            usd[i] = True
+            perm.append(nums[i])
+            helper(perm, usd)
+            perm.pop()
+            usd[i] = False
+
+    result = []
+    if not nums:
+        return result
+    used = [False for i in range(len(nums))]
+    permutation = []
+    helper(permutation, used)
+    return result
+
+
+inp = [1, 2]
+exp = [[1, 2], [2, 1]]
+print(permute(inp) == exp)
+
+inp = [1, 2, 3]
+exp = [
+            [1, 2, 3],
+            [1, 3, 2],
+            [2, 1, 3],
+            [2, 3, 1],
+            [3, 1, 2],
+            [3, 2, 1]
+        ]
+print(permute(inp) == exp)
