e90768cdb98f61895119e28e33d3412f0fa74c25
TerryLun <tianweilun@yahoo.com>
2020-03-29 12:55:36 -0700

    Leetcode #70. Climbing Stairs memoized

--- lc70e.py
+++ lc70e.py
@@ -6,28 +6,40 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 """
 
 
+def memoize(f):
+    memo = {}
+
+    def helper(n):
+        if n not in memo:
+            memo[n] = f(n)
+        return memo[n]
+
+    return helper
+
+
+@memoize
 def climbStairs(n):
     """
     :type n: int
     :rtype: int
     """
-    # # recursion
-    # if n == 1 or n == 0:
-    #     return 1
-    # if n == 2:
-    #     return 2
-    # return climbStairs(n - 1) + climbStairs(n - 2)
+    # recursion
+    if n == 1 or n == 0:
+        return 1
+    if n == 2:
+        return 2
+    return climbStairs(n - 1) + climbStairs(n - 2)
 
-    climb = [1, 1, 2]
-    for i in range(3, n + 1):
-        climb.append(climb[i - 1] + climb[i - 2])
-    return climb[n]
+    # climb = [1, 1, 2]
+    # for i in range(3, n + 1):
+    #     climb.append(climb[i - 1] + climb[i - 2])
+    # return climb[n]
 
 
 inp = 2
 exp = 2
 print(climbStairs(inp) == exp)
 
-inp = 30
-exp = 1346269
+inp = 300
+exp = 359579325206583560961765665172189099052367214309267232255589801
 print(climbStairs(inp) == exp)
