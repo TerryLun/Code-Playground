436e179f9217d74c393b7f32041040855d88eba0
TerryLun <tianweilun@yahoo.com>
2020-04-01 15:28:14 -0700

    Leetcode #191. Number of 1 Bits

---
+++ lc191e.py
@@ -0,0 +1,18 @@
+"""
+191. Number of 1 Bits
+
+Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming
+weight).
+"""
+
+
+def hammingWeight(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    count = 0
+    while n != 0:
+        n = n & (n - 1)
+        count += 1
+    return count
