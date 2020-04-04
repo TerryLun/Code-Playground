d7fc73325904536cc1b8e7d4d6627b742e03f20b
TerryLun <tianweilun@yahoo.com>
2020-04-04 14:48:49 -0700

    Leetcode #283. Move Zeroes

--- lc283e.py
+++ lc283e.py
@@ -19,16 +19,10 @@ def moveZeroes(nums):
     i = 0
     j = 0
     while j < len(nums):
-        if nums[i] != 0:
+        if nums[j] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
             i += 1
-        elif nums[j] != 0:
-            nums[i] = nums[j]
-            i += 1
-            nums[i] = 0
         j += 1
-    while i < j:
-        nums[i] = 0
-        i += 1
 
 
 nums = [0, 1, 0, 3, 12]
