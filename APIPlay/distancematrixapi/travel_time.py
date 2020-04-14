e90633841cd7cd2e6317dfe03c52711cf223297a
TerryLun <tianweilun@yahoo.com>
2020-04-13 22:21:54 -0700

    distance matrix api

---
+++ APIPlay/distancematrixapi/travel_time.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+def get_travel_time():
+    """
+    Query user input address then return latitude and longitude
+    """
+    origins = '49.282441,-123.118667'
+    destinations = '3700 Number 3 Rd, Richmond, BC V6X 3X2, Canada'
+    mode = 'driving'
+    key = 'AIzaSyARtxtrUYeHG4cfM7O5TRpDy62FG7lkszg'
+    responses = requests.get(f'https://maps.googleapis.com/maps/api/distancematrix/json?origins={origins}&destinations={destinations}&mode={mode}&language=en-CA&key={key}')
+    responses.raise_for_status()
+    data = json.loads(responses.text)
+    distance = data['rows'][0]['elements'][0]['distance']['text']
+    travel_duration = data['rows'][0]['elements'][0]['duration']['text']
+    return distance, travel_duration
+
+
+def main():
+    print('Loaded: travel_time.py')
+
+
+if __name__ == "__main__":
+    main()
