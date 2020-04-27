b30deaa7e1c9f4f5b1f88ef873e5ab73792c4ea0
TerryLun <47690039+TerryLun@users.noreply.github.com>
2020-04-27 14:41:47 -0700

    Delete sms.py

--- APIPlay/Nexmo/sms.py
+++
@@ -1,9 +0,0 @@
-import nexmo
-
-client = nexmo.Client(key='29492de1', secret='UoZqHZhgB9hRHpTt')
-
-client.send_message({
-    'from': '14372662415',
-    'to': '1778_______',
-    'text': 'Text notification test',
-})
