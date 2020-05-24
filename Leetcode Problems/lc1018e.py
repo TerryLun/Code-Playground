95aa20d8f31c8c4ab094790b4b7b6432d29fa80e
TerryLun <tianweilun@yahoo.com>
2020-05-23 21:46:23 -0700

    Leetcode #1018. Binary Prefix Divisible By 5

--- Leetcode Problems/lc1018e.py
+++ Leetcode Problems/lc1018e.py
@@ -6,6 +6,7 @@ Given an array A of 0s and 1s, consider N_i: the i-th subarray from A[0] to A[i]
 
 Return a list of booleans answer, where answer[i] is true if and only if N_i is divisible by 5.
 
+
 Note:
 1 <= A.length <= 30000
 A[i] is 0 or 1
