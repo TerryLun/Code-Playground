13ad8b97266a3ae66ac878fb4bbb2f2b88a96d37
TerryLun <tianweilun@yahoo.com>
2020-05-26 23:03:10 -0700

    Leetcode #118. Pascal's Triangle

--- Leetcode Problems/lc118e.py
+++ Leetcode Problems/lc118e.py
@@ -1,5 +1,6 @@
 """
 118. Pascal's Triangle
+
 Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
 """
