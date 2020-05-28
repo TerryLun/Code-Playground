dc18c42748c2bdd3547f59f3bfb5b10c2898817b
TerryLun <tianweilun@yahoo.com>
2020-05-27 20:09:23 -0700

    Leetcode #1232. Check If It Is a Straight Line

--- Leetcode Problems/lc1232e.py
+++ Leetcode Problems/lc1232e.py
@@ -4,6 +4,7 @@
 You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point.
 Check if these points make a straight line in the XY plane.
 
+
 Constraints:
 2 <= coordinates.length <= 1000
 coordinates[i].length == 2
