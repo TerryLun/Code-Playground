c6b61fbf22a2954a0b4b1fedeb6133522365ec85
TerryLun <tianweilun@yahoo.com>
2020-05-30 17:25:36 -0700

    Leetcode #1360. Number of Days Between Two Dates

--- Leetcode Problems/lc1360e.py
+++ Leetcode Problems/lc1360e.py
@@ -4,6 +4,7 @@
 Write a program to count the number of days between two dates.
 The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
 
+
 Constraints:
 The given dates are valid dates between the years 1971 and 2100.
 """
