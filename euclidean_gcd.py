767b67c546de184b1ecd60db31f9444ab470761f
TerryLun <tianweilun@yahoo.com>
2020-05-31 19:29:28 -0700

    Euclidean algorithm for gcd

--- euclidean_gcd.py
+++ euclidean_gcd.py
@@ -6,4 +6,4 @@ https://en.wikipedia.org/wiki/Greatest_common_divisor
 def gcd(num1, num2):
     while num1 % num2 != 0:
         num1, num2 = num2, num1 % num2
-    return num2
+    return abs(num2)
