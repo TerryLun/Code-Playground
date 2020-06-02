7e2315cf4abbf657b2f169f96bf7fcdd02c8b580
Terry <tianweilun@yahoo.com>
2020-06-02 16:23:03 -0700

    Birthday Chocolate

---
+++ HackerRank Problems/birthday_chocolate.py
@@ -0,0 +1,17 @@
+def birthday(s, d, m):
+    if m > len(s):
+        return 0
+    window_sum = sum(s[:m])
+    result = 0
+    if window_sum == d:
+        result += 1
+    for i in range(1, len(s)-m+1):
+        window_sum += s[i+m-1] - s[i-1]
+        print(s[i+m-1], s[i-1])
+        if window_sum == d:
+            result += 1
+    print(result)
+    return result
+
+
+birthday([1, 2, 1, 3, 2], 3, 2)
