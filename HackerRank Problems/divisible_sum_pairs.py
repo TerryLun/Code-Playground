9c60a750ed39ca1d49e56ba70d1e3893d55522fb
TerryLun <tianweilun@yahoo.com>
2020-06-03 20:20:37 -0700

    Divisible Sum Pairs

--- HackerRank Problems/divisible_sum_pairs.py
+++ HackerRank Problems/divisible_sum_pairs.py
@@ -1,4 +1,11 @@
+import random
+import time
+
+
 def divisibleSumPairs(n, k, ar):
+    """
+    O(n)
+    """
     hi = max(ar)
     lo = min(ar)
     d = {}
@@ -13,7 +20,32 @@ def divisibleSumPairs(n, k, ar):
                 d[f] = 1
         for b in range(-num, lo - 1, -k):
             if b in d:
-                b[f] += 1
+                d[b] += 1
             else:
-                b[f] = 1
+                d[b] = 1
     return result
+
+
+def bruteForce(n, k, ar):
+    """
+    O(n*n)
+    """
+    result = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (ar[i] + ar[j]) % k == 0:
+                result += 1
+    return result
+
+
+inp = [random.randint(1, 10) for _ in range(10000)]
+k = 3
+n = len(inp)
+
+start = time.time()
+print(divisibleSumPairs(n, k, inp))
+print(time.time() - start)
+
+start = time.time()
+print(bruteForce(n, k, inp))
+print(time.time() - start)
