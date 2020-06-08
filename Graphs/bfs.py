ea62d7c71f6dee63a6cb537150556db621fcfbf8
TerryLun <tianweilun@yahoo.com>
2020-06-07 21:43:18 -0700

    Simple bfs

---
+++ Graphs/bfs.py
@@ -0,0 +1,48 @@
+# Python3 Program to print BFS traversal
+# from a given source vertex. BFS(int s)
+# traverses vertices reachable from s.
+from collections import defaultdict
+
+
+# This class represents a directed graph
+# using adjacency list representation
+class Graph:
+
+    # Constructor
+    def __init__(self):
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    # Function to print a BFS of graph
+    def bfs(self, s):
+        queue = [s]
+        visited = [False] * len(self.graph)
+
+        visited[s] = True
+        while queue:
+            node = queue.pop(0)
+            print(node, end=' ')
+            for n in self.graph[node]:
+                if not visited[n]:
+                    queue.append(n)
+                    visited[n] = True
+
+
+# Driver code
+
+# Create a graph given in the above diagram
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is Breadth First Traversal"
+      " (starting from vertex 2)")
+g.bfs(2)
