e32fccca6132f4aa3f35d6b39998aa2b98d9aeef
TerryLun <tianweilun@yahoo.com>
2020-06-09 00:50:03 -0700

    Simple dfs

---
+++ Graphs/dfs.py
@@ -0,0 +1,46 @@
+# Python3 program to print DFS traversal
+# from a given given graph
+from collections import defaultdict
+
+
+# This class represents a directed graph using
+# adjacency list representation
+class Graph:
+
+    # Constructor
+    def __init__(self):
+
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+        # function to add an edge to graph
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def DFSUtil(self, v, visited):
+        visited[v] = True
+        print(v, end=' ')
+        for i in self.graph[v]:
+            if not visited[i]:
+                self.DFSUtil(i, visited)
+
+    def DFS(self, v):
+        visited = [False] * len(self.graph)
+        self.DFSUtil(v, visited)
+
+
+# Driver code
+
+# Create a graph given
+# in the above diagram
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is DFS from (starting from vertex 2)")
+g.DFS(2)
