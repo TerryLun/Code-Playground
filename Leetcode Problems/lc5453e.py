43fb9862618b6d58774152b3a16641be61cc9598
TerryLun <tianweilun@yahoo.com>
2020-06-13 21:06:20 -0700

    LeetCode #5453. Running Sum of 1d Array (Contest #193)

---
+++ Leetcode Problems/lc5453e.py
@@ -0,0 +1,12 @@
+"""
+5453. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+"""
+
+def runningSum(nums):
+    for i in range(1, len(nums)):
+        nums[i] += nums[i - 1]
+    return nums
+
