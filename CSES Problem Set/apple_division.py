2e0d0a0d2e89acd8317592b3f4009bfa03378e90
TerryLun <tianweilun@yahoo.com>
2020-06-13 23:20:11 -0700

    Apple Division

--- CSES Problem Set/apple_division.py
+++ CSES Problem Set/apple_division.py
@@ -1,19 +1,13 @@
-"""
-TC7-12 TLE
-"""
-
 import itertools
 import math
-import copy
 
 n = int(input())
 a = list(map(int, input().split(' ')))
+s = sum(a)
 md = float('inf')
 for i in range(1, math.ceil(n / 2) + 1):
     c = itertools.combinations(a, i)
     for left in c:
-        right = copy.deepcopy(a)
-        for leftn in left:
-            right.remove(leftn)
-        md = min(md, abs(sum(left) - sum(right)))
+        sl = sum(left)
+        md = min(md, abs(sl * 2 - s))
 print(md)
