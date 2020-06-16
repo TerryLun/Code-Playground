d44c605b9a2c61b210f445ead612db7df617d515
TerryLun <tianweilun@yahoo.com>
2020-06-15 20:26:06 -0700

    Prime Factors

---
+++ HackerRank Problems/prime_factor.py
@@ -0,0 +1,22 @@
+import math
+
+
+def prime_factors(n):
+    s = []
+    # 2s
+    while n % 2 == 0:
+        s.append(2)
+        n //= 2
+    # from 3 and above
+    for i in range(3, math.isqrt(n) + 1, 2):
+        if n % i == 0:
+            while n % i == 0:
+                s.append(i)
+                n //= i
+    # if n is a prime number greater than 2
+    if n > 2:
+        s.append(n)
+    return s
+
+
+print(prime_factors(144))
