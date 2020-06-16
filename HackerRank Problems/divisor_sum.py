2ae1b9a01dc0b9540d7c539df24ba95d3b305a0d
TerryLun <tianweilun@yahoo.com>
2020-06-15 20:48:49 -0700

    Divisor Sum

--- HackerRank Problems/divisor_sum.py
+++ HackerRank Problems/divisor_sum.py
@@ -1,11 +1,36 @@
-n = int(input())
-i = 1
-s = 0
-while i * i <= n:
-    if n % i == 0:
-        if n // i == i:
-            s += i
-        else:
-            s += i + n // i
-    i += 1
-print(s)
+import prime_factor
+import time
+
+
+def divisor_sum_in_pair(n):
+    i = 1
+    s = 0
+    while i * i <= n:
+        if n % i == 0:
+            if n // i == i:
+                s += i
+            else:
+                s += i + n // i
+        i += 1
+    return s
+
+
+def divisor_sum_using_prime_factors(n):
+    pf = list(set(prime_factor.prime_factors(n)))
+    s = 1
+    for p in pf:
+        k = 0
+        while n % p == 0:
+            k += 1
+            n //= p
+        s *= (p ** (k + 1) - 1) // (p - 1)
+    return s
+
+
+s = time.time()
+print(divisor_sum_in_pair(180000000000000))
+print(time.time() - s)
+
+s = time.time()
+print(divisor_sum_using_prime_factors(180000000000000))
+print(time.time() - s)
