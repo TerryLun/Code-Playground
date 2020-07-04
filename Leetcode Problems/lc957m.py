c3b7bbd867fac36b2e742e694df21e35eb156373
TerryLun <tianweilun@yahoo.com>
2020-07-03 19:48:00 -0700

    LeetCode #957. Prison Cells After N Days

--- Leetcode Problems/lc957m.py
+++ Leetcode Problems/lc957m.py
@@ -28,24 +28,18 @@ def prisonAfterNDays(cells, N):
     length of loop is 14 or 7 - Accepted
     """
     N = (N % 14) + 14
-    memo = {}
     for d in range(N):
-        s = ''.join(list(map(str, cells)))
-        if s in memo:
-            cells[:] = memo[s]
-        else:
-            a = cells[:]
-            for i in range(1, len(cells) - 1):
-                if cells[i - 1] == cells[i + 1]:
-                    a[i] = 1
-                else:
-                    a[i] = 0
-            if d == 0 and cells[0] == 1:
-                a[0] = 0
-            if d == 0 and cells[-1] == 1:
-                a[-1] = 0
-            memo[s] = a[:]
-            cells[:] = a[:]
+        a = cells[:]
+        for i in range(1, len(cells) - 1):
+            if cells[i - 1] == cells[i + 1]:
+                a[i] = 1
+            else:
+                a[i] = 0
+        if d == 0 and cells[0] == 1:
+            a[0] = 0
+        if d == 0 and cells[-1] == 1:
+            a[-1] = 0
+        cells[:] = a[:]
     return cells
 
 
