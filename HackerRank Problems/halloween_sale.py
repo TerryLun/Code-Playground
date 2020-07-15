a6bb1c4262c865997dec2a03e0934fee2b31cb16
TerryLun <tianweilun@yahoo.com>
2020-07-14 22:00:58 -0700

    Halloween Sale

---
+++ HackerRank Problems/halloween_sale.py
@@ -0,0 +1,11 @@
+def howManyGames(p, d, m, s):
+    r = 0
+    while s > 0:
+        s -= p
+        p = max(p - d, m)
+        r += 1
+        print(s)
+    return r if s == 0 else r - 1
+
+
+print(howManyGames(20, 3, 6, 80))
