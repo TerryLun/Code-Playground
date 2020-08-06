0299aff9d92ac028927298545ca33a83611327b5
TerryLun <tianweilun@yahoo.com>
2020-08-06 16:22:47 -0700

    Super Reduced String

---
+++ HackerRank Problems/super_reduced_string.py
@@ -0,0 +1,14 @@
+"""
+https://www.hackerrank.com/challenges/reduced-string/problem
+"""
+
+
+def superReducedString(s):
+    i = 0
+    while i < len(s) - 1:
+        if s[i] == s[i + 1]:
+            s = s[:i] + s[i + 2:]
+            i = 0
+        else:
+            i += 1
+    return s if s else 'Empty String'
