8f752990fe84b2478beae17bdb1f2b840a90153b
TerryLun <tianweilun@yahoo.com>
2020-08-05 23:52:43 -0700

    LeetCode #453. Minimum Moves to Equal Array Elements

--- Leetcode Problems/lc453e.py
+++ Leetcode Problems/lc453e.py
@@ -21,7 +21,8 @@ def minMoves(nums):
     #     count += 1
     # return count
     """
-    let's define sum as the sum of all the numbers, before any moves; minNum as the min number int the list; n is the length of the list;
+    let's define sum as the sum of all the numbers, before any moves; minNum as the min number int the list; n is the 
+    length of the list;
 
     After, say m moves, we get all the numbers as x , and we will get the following equation
     
@@ -31,10 +32,13 @@ def minMoves(nums):
     
          x = minNum + m
       
-    This part may be a little confusing, but @shijungg explained very well. let me explain a little again. it comes from two observations:
+    This part may be a little confusing, but @shijungg explained very well. let me explain a little again. it comes from 
+    two observations:
     
-    the minum number will always be minum until it reachs the final number, because every move, other numbers (besides the max) will be increamented too;
-    from above, we can get, the minum number will be incremented in every move. So, if the final number is x, it would be minNum + moves;
+    the minum number will always be minum until it reachs the final number, because every move, other numbers (besides 
+    the max) will be increamented too;
+    from above, we can get, the minum number will be incremented in every move. So, if the final number is x, it would 
+    be minNum + moves;
     and finally, we will get
     
         sum - minNum * n = m
