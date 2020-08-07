5e80590b5e4ec73c2ef75dbd8bd9002dc1c8c5bf
TerryLun <tianweilun@yahoo.com>
2020-08-07 11:30:35 -0700

    Super Reduced String

--- HackerRank Problems/super_reduced_string.py
+++ HackerRank Problems/super_reduced_string.py
@@ -8,7 +8,7 @@ def superReducedString(s):
     while i < len(s) - 1:
         if s[i] == s[i + 1]:
             s = s[:i] + s[i + 2:]
-            i = 0
+            i -= 1 if i > 0 else i
         else:
             i += 1
     return s if s else 'Empty String'
