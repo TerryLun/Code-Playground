f5013c48534b9991afe6f329281185e50de56428
TerryLun <tianweilun@yahoo.com>
2020-08-18 20:56:38 -0700

    LeetCode #709. To Lower Case

---
+++ Leetcode Problems/lc709e.py
@@ -0,0 +1,9 @@
+"""
+709. To Lower Case
+
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+"""
+
+
+def toLowerCase(s):
+    return s.lower()
