54d8fe6e5e90299457160ba28bd7bd303e926c2b
TerryLun <tianweilun@yahoo.com>
2020-08-22 20:23:31 -0700

    LeetCode #643. Maximum Average Subarray I

--- Leetcode Problems/lc643e.py
+++ Leetcode Problems/lc643e.py
@@ -10,11 +10,6 @@ Elements of the given array will be in the range [-10,000, 10,000].
 
 
 def findMaxAverage(nums, k):
-    """
-    :type nums: List[int]
-    :type k: int
-    :rtype: float
-    """
     sums = sum(nums[:k])
     maxs = sums
     for i in range(k, len(nums)):
