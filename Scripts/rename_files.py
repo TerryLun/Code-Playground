33234315e70db89dd68c6854c264cc138efdd6a5
TerryLun <tianweilun@yahoo.com>
2020-09-02 23:00:25 -0700

    rename files

--- Scripts/rename_files.py
+++ Scripts/rename_files.py
@@ -1,8 +1,10 @@
 import os
 
 FILENAME_STARTS = 'WeC'
+NAME_LENGTH = 3
+
 count = 1
 for file in os.listdir():
     if file.startswith(FILENAME_STARTS):
-        os.rename(file, f'{str(count).rjust(3, "0")}.png')
+        os.rename(file, f'{str(count).rjust(NAME_LENGTH, "0")}.png')
         count += 1
