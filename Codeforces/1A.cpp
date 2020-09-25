757663f08b9aaa4c4b8059b1765c750d32e197b3
Terry <tianweilun@yahoo.com>
2020-09-25 11:51:43 -0700

    1A

---
+++ Codeforces/1A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+ 
+using namespace std;
+ 
+#define ar array
+#define ll long long
+ 
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+ 
+ 
+ 
+void solve() {
+    double n,m,a;
+    cin>>n>>m>>a;
+    cout<<(ll)ceil(n/a)*(ll)ceil(m/a)<<endl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+ 
+    // int tc; cin >> tc;
+    // for (int t = 1; t <= tc; t++) {
+    //     // cout << "Case #" << t  << ": ";
+    //     solve();
+    // }
+    solve();
+}
