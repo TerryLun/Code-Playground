a11a186bd2de0ee703be221e2f6bdc2d72e97132
Terry <tianweilun@yahoo.com>
2020-10-29 02:02:31 -0700

    construct tower

--- Clash of Code/construct_tower.py
+++ Clash of Code/construct_tower.py
@@ -1,11 +1,5 @@
 """
-test cases:
-
-in:
-257
-3 7
-out:
-83
+sample test case:
 
 in:
 257
@@ -14,6 +8,9 @@ out:
 83
 """
 
+"""
+dp
+"""
 import sys
 
 sys.setrecursionlimit(int(1e7))
@@ -36,3 +33,25 @@ def dp(n):
 
 
 print(max(0, dp(n)))
+
+"""
+simple
+"""
+big = 0
+small = 0
+
+while n > 0:
+    if n % p == 0:
+        small = n // p
+        break
+    else:
+        if (n - p > 0):
+            n -= q
+            big += 1
+        else:
+            break
+
+if (n % p != 0):
+    print(0)
+else:
+    print(small + big)
