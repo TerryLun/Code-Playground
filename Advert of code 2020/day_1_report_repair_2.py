a0a7cd0cd1f33c3795fd9143a5926ae65971e21a
Terry <tianweilun@yahoo.com>
2020-12-05 14:01:27 -0800

    Day 1: Report Repair Part 2

--- Advert of code 2020/day_1_report_repair_2.py
+++ Advert of code 2020/day_1_report_repair_2.py
@@ -5,9 +5,21 @@ while 1:
         break
     a += [int(line)]
 
-s = {a[0]}
-for n in a:
-    for m in s:
-        s.add(n * m)
-    if 2020-n in s:
-        print(n*(2020-n))
+print("input get")
+
+
+def get_sum(l, num):
+    x = set()
+    for n in l:
+        if n in x:
+            return n, num - n
+        else:
+            x.add(num - n)
+    return (-1, -1)
+
+
+for i, n in enumerate(a):
+    z = get_sum(a[:i] + a[i:], 2020 - n)
+    if z[0] != -1:
+        print(n * z[0] * z[1])
+        exit()
