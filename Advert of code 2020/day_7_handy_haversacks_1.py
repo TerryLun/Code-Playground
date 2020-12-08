3ce716f7ef896ec0d976f8a028fa6c13f6eb291d
Terry <tianweilun@yahoo.com>
2020-12-08 02:16:34 -0800

    Day 7: Handy Haversacks - Parse input

---
+++ Advert of code 2020/day_7_handy_haversacks_1.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+r = 0
+bags = {}
+
+while 1:
+    line = input()
+    if not line:
+        break
+    main_bag, right = line.split(' contain ')
+    right = right.replace('.', '')
+
+    small_bags = right.split(', ')
+    bags_with_num = {}
+    for bag in small_bags:
+        num, bag_name = bag.split(' ', 1)
+        try:
+            bags_with_num[bag_name] = int(num)
+        except ValueError:
+            bags[main_bag] = {}
+            break
+    bags[main_bag] = bags_with_num.copy()
+    bags_with_num.clear()
+
+pprint(bags, depth=2)
